519eded7a7eb37d04e6019f025fa79914bb015e2
elokoidu <elo.koidu@tptlive.ee>
2020-08-22 23:34:44 +0300

    Revert "file update"
    This reverts commit 28623762364c61b80b22b29f8f055e21d36516f5.

--- database/migrations/2020_08_22_140856_create_products_table.php
+++ database/migrations/2020_08_22_140856_create_products_table.php
@@ -19,7 +19,6 @@ class CreateProductsTable extends Migration
             $table->float('hind');
             $table->float('tootekood');
             $table->binary('tootefoto');
-            $table->string('naitajad');
             $table->string('tootja');
             $table->enum('kategooria', ['Monitor', 'Lisatarvikud', 'Emaplaat', 'Kõvaketas', 'Graafikakaart']);
         });
