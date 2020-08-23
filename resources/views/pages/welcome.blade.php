53d0694f38847f66bf2335d32915032bd4c08102
elokoidu <elo.koidu@tptlive.ee>
2020-08-24 00:58:02 +0300

    Final for now

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('title', '| Avaleht')
 @section('content')
     @include('layouts.hero')
-    <div class="uk-grid uk-margin-large-left">
+    <div class="uk-grid uk-margin-large-left@m">
     @include('partials._item')
     </div>
 @endsection
